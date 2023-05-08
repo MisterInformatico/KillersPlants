cad583c4ed8fec74267f9f20f8ad4b026b243713
MisterInformatico <132946983+MisterInformatico@users.noreply.github.com>
2023-05-08 15:50:00 -0400

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,26 @@
+# Variables de la Pantalla
+anchoPantalla = 800
+altoPantalla = 600
+sizePantalla = (anchoPantalla, altoPantalla)
+FPS = 60
+ESCALA = 3
+ESCALA_ARMA = 1.5
+ESCALA_ARTICULOS = 3
+ESCALA_POSION = 2
+OFFSET = 12
+
+CASILLAS_SIZE = 16 * ESCALA
+CASILLAS_TYPE = 18
+# Columnas y Filas de los leves
+FILAS = 150
+COLUMNAS = 150
+
+# Colores
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+FONDO = (40, 25, 25)
+PANEL = (50, 50, 50)
+
+# Fisicas
+VELOCIDAD = 5
+VELOCIDAD_FLECHA = 10
